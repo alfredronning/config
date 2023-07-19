bf58ae8b109a3e66bf823f6ace273bbad0ce7878
Alfred Sollie Rønning <alfredronning@gmail.com>
2023-07-20 01:35:34 +0200

    backup gammel config

--- initsetup.sh
+++ initsetup.sh
@@ -3,7 +3,14 @@
 declare -a configs=(".bashrc" ".config/nvim" ".tmux.conf" ".vimrc" ".Xmodmap" ".zshrc")
 
 for config in ${configs[@]}; do
-    rm -rf ${HOME}/$config
-    ln -s ${PWD}/$config ${HOME}/$config
+    FILE=${HOME}/$config
+    TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+    if test -f "$FILE" || test -d "$FILE"; then
+        mkdir -p backup/$TIMESTAMP
+        cp -r $FILE ${PWD}/backup/$FILE_$TIMESTAMP
+        echo $FILE alredy exist. Saved backup of file in ${PWD}/backup
+    fi
+    rm -rf $FILE
+    ln -s ${PWD}/$config $FILE
 done
 
