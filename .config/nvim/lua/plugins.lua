c258b5e6eb2c655f0c184adbfaa6fa31c5b1ce14
Alfred Sollie Rønning <alfredronning@gmail.com>
2023-07-20 00:32:21 +0200

    fiks ripgrep

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,9 +6,12 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        requires = {
+            {'nvim-lua/plenary.nvim'},
+            {'BurntSushi/ripgrep'}
+        }
     }
 
     use({
