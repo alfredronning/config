9a8c588ff6148ba74330f662784b50bf876b7e72
Alfred Sollie Rønning <alfredronning@gmail.com>
2023-07-23 12:57:02 +0200

    Install dependencies and sync packages init

--- initsetup.sh
+++ initsetup.sh
@@ -2,15 +2,33 @@
 
 declare -a configs=(".bashrc" ".config/nvim" ".tmux.conf" ".vimrc" ".Xmodmap" ".zshrc")
 
+# create symlinks to configs in home
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 for config in ${configs[@]}; do
     FILE=${HOME}/$config
-    TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
     if test -f "$FILE" || test -d "$FILE"; then
         mkdir -p backup/$TIMESTAMP
-        cp -r $FILE ${PWD}/backup/$FILE_$TIMESTAMP
-        echo $FILE alredy exist. Saved backup of file in ${PWD}/backup
+        cp -r $FILE backup/$TIMESTAMP
+        echo $FILE alredy exist. Saved backup of file in ./backup
+        rm -rf $FILE
     fi
-    rm -rf $FILE
     ln -s ${PWD}/$config $FILE
 done
 
+# install pyright
+if [ `npm list -g | grep -c pyright` -eq 0 ]; then
+    sudo npm install -g pyright
+fi
+
+# install ripgrep
+if [ $(dpkg-query -W -f='${Status}' ripgrep 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+    sudo apt install ripgrep
+fi
+
+# install and sync nvim packer
+if test ! -d ~/.local/share/nvim/site/pack/packer; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
